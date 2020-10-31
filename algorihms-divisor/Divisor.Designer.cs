4bf1f5aefd9042fd9e884930069995fefffc7ba0
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-10-31 16:33:49 +0200

    algorithms-divisor

---
+++ algorihms-divisor/Divisor.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace algorihms_divisor
+{
+    partial class Divisor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.listHesapla = new System.Windows.Forms.ListBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAsal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Girilen sayının tam bölünenlerini bulma algoritması.";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClear.Location = new System.Drawing.Point(306, 263);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(52, 33);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // listHesapla
+            // 
+            this.listHesapla.FormattingEnabled = true;
+            this.listHesapla.Location = new System.Drawing.Point(309, 97);
+            this.listHesapla.Name = "listHesapla";
+            this.listHesapla.Size = new System.Drawing.Size(161, 160);
+            this.listHesapla.TabIndex = 9;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNumber.Location = new System.Drawing.Point(309, 71);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(161, 23);
+            this.txtNumber.TabIndex = 8;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNumber.Location = new System.Drawing.Point(259, 74);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(47, 17);
+            this.lblNumber.TabIndex = 7;
+            this.lblNumber.Text = "Sayı : ";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnHesapla.Location = new System.Drawing.Point(364, 263);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(56, 33);
+            this.btnHesapla.TabIndex = 6;
+            this.btnHesapla.Text = "Hasapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Minion Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(306, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "00";
+            // 
+            // btnAsal
+            // 
+            this.btnAsal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnAsal.Location = new System.Drawing.Point(426, 263);
+            this.btnAsal.Name = "btnAsal";
+            this.btnAsal.Size = new System.Drawing.Size(49, 33);
+            this.btnAsal.TabIndex = 13;
+            this.btnAsal.Text = "Asal";
+            this.btnAsal.UseVisualStyleBackColor = false;
+            this.btnAsal.Click += new System.EventHandler(this.btnAsal_Click);
+            // 
+            // Divisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::algorihms_divisor.Properties.Resources.c78;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(549, 363);
+            this.Controls.Add(this.btnAsal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listHesapla);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Divisor";
+            this.Text = "Sayının Bölünenleri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox listHesapla;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAsal;
+    }
+}
+
---
+++ algorihms-divisor/Divisor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace algorihms_divisor
+{
+    public partial class Divisor : Form
+    {
+        public Divisor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listHesapla.Items.Clear();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+
+            int number = Convert.ToInt32(txtNumber.Text);
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    listHesapla.Items.Add(i);
+                }
+               
+            }
+
+        }
+
+        private void btnAsal_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            int number = Convert.ToInt32(txtNumber.Text);
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    index++;
+                }
+                if (index == 0)
+                {
+                    label2.Text = number + " : sayısı asaldır.";
+                }
+                else
+                {
+                    label2.Text = number + " : sayısı asal değildir.";
+                }
+            }
+        }
+    }
+}
