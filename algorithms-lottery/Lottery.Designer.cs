18a500bc0833227d137acf3bc9049fa9106d7629
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-10-31 15:50:09 +0200

    algorithms-lotery

---
+++ algorithms-lottery/Lottery.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace algorithms_lottery
+{
+    partial class Lottery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listNumber = new System.Windows.Forms.ListBox();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnSayisal = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTik = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listNumber
+            // 
+            this.listNumber.Font = new System.Drawing.Font("MingLiU-ExtB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listNumber.FormattingEnabled = true;
+            this.listNumber.ItemHeight = 20;
+            this.listNumber.Location = new System.Drawing.Point(441, 12);
+            this.listNumber.Name = "listNumber";
+            this.listNumber.Size = new System.Drawing.Size(169, 224);
+            this.listNumber.TabIndex = 15;
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl6.Location = new System.Drawing.Point(337, 100);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(23, 25);
+            this.lbl6.TabIndex = 14;
+            this.lbl6.Text = "0";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl5.Location = new System.Drawing.Point(262, 100);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(23, 25);
+            this.lbl5.TabIndex = 13;
+            this.lbl5.Text = "0";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl4.Location = new System.Drawing.Point(188, 100);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(23, 25);
+            this.lbl4.TabIndex = 12;
+            this.lbl4.Text = "0";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(337, 43);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(23, 25);
+            this.lbl3.TabIndex = 11;
+            this.lbl3.Text = "0";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(262, 43);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(23, 25);
+            this.lbl2.TabIndex = 10;
+            this.lbl2.Text = "0";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(188, 43);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(23, 25);
+            this.lbl1.TabIndex = 9;
+            this.lbl1.Text = "0";
+            // 
+            // btnSayisal
+            // 
+            this.btnSayisal.BackColor = System.Drawing.Color.Red;
+            this.btnSayisal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSayisal.ForeColor = System.Drawing.Color.White;
+            this.btnSayisal.Location = new System.Drawing.Point(168, 163);
+            this.btnSayisal.Name = "btnSayisal";
+            this.btnSayisal.Size = new System.Drawing.Size(117, 49);
+            this.btnSayisal.TabIndex = 8;
+            this.btnSayisal.Text = "Sayısal Oyna";
+            this.btnSayisal.UseVisualStyleBackColor = false;
+            this.btnSayisal.Click += new System.EventHandler(this.btnSayisal_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(291, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 49);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Temizle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTik
+            // 
+            this.lblTik.AutoSize = true;
+            this.lblTik.Font = new System.Drawing.Font("Minion Pro", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTik.Location = new System.Drawing.Point(21, 40);
+            this.lblTik.Name = "lblTik";
+            this.lblTik.Size = new System.Drawing.Size(23, 19);
+            this.lblTik.TabIndex = 17;
+            this.lblTik.Text = "00";
+            // 
+            // Lottery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::algorithms_lottery.Properties.Resources.sayısal_loto;
+            this.ClientSize = new System.Drawing.Size(750, 359);
+            this.Controls.Add(this.lblTik);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listNumber);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnSayisal);
+            this.Name = "Lottery";
+            this.Text = "Sayısal Loto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listNumber;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnSayisal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTik;
+    }
+}
+
---
+++ algorithms-lottery/Lottery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace algorithms_lottery
+{
+    public partial class Lottery : Form
+    {
+        int click = 0;
+        public Lottery()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayisal_Click(object sender, EventArgs e)
+        {
+            click++;
+            Random random = new Random();
+            int[] rastgele = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                rastgele[i] = random.Next(1, 50);
+                listNumber.Items.Add(rastgele[i]);
+
+                if (click % 2 == 1 || click % 2 == 0)
+                {
+                    lblTik.Text = "Oynanan Kolon : " + click;
+                }
+            }
+            listNumber.Items.Add("**********");
+            //Array.Sort(rastgele); İstersek böyle sıralama yapabiliriz.
+
+            lbl1.Text = rastgele[0].ToString();
+            lbl2.Text = rastgele[1].ToString();
+            lbl3.Text = rastgele[2].ToString();
+            lbl4.Text = rastgele[3].ToString();
+            lbl5.Text = rastgele[4].ToString();
+            lbl6.Text = rastgele[5].ToString();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listNumber.Items.Clear();
+        }
+    }
+}
