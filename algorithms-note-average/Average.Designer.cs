b04ca2828d9d6b97ba400df2c7a939ec65b19480
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-01 12:09:19 +0200

    algorithms-note-average

---
+++ algorithms-note-average/Average.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace algorithms_note_average
+{
+    partial class Average
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrtalama = new System.Windows.Forms.Button();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOrtalama
+            // 
+            this.btnOrtalama.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnOrtalama.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrtalama.Location = new System.Drawing.Point(186, 129);
+            this.btnOrtalama.Name = "btnOrtalama";
+            this.btnOrtalama.Size = new System.Drawing.Size(119, 71);
+            this.btnOrtalama.TabIndex = 0;
+            this.btnOrtalama.Text = "Ortalama";
+            this.btnOrtalama.UseVisualStyleBackColor = false;
+            this.btnOrtalama.Click += new System.EventHandler(this.btnOrtalama_Click);
+            // 
+            // txtVize
+            // 
+            this.txtVize.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtVize.Location = new System.Drawing.Point(25, 129);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(131, 33);
+            this.txtVize.TabIndex = 1;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFinal.Location = new System.Drawing.Point(25, 167);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(131, 33);
+            this.txtFinal.TabIndex = 2;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblTotal.Font = new System.Drawing.Font("Minion Pro SmBd", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotal.Location = new System.Drawing.Point(28, 216);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(51, 33);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::algorithms_note_average.Properties.Resources.dokuz;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(345, 400);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.btnOrtalama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrtalama;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ algorithms-note-average/Average.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace algorithms_note_average
+{
+    public partial class Average : Form
+    {
+        public Average()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrtalama_Click(object sender, EventArgs e)
+        {
+            int ortalama, vize, final;
+
+            final = Convert.ToInt32(txtFinal.Text);
+            vize = Convert.ToInt32(txtVize.Text);
+
+            ortalama = vize + final;
+            ortalama= ortalama / 2;
+
+            if (ortalama <= 100 && ortalama >= 85)
+            {
+                lblTotal.Text = "Ortalamanız : " + ortalama + " Notunuz : " + 5;
+            }
+            else if (ortalama < 85 && ortalama >= 70)
+            {
+                lblTotal.Text = "Ortalamanız : " + ortalama + " Notunuz : " + 4;
+            }
+            else if (ortalama < 70 && ortalama >= 55)
+            {
+                lblTotal.Text = "Ortalamanız : " + ortalama + " Notunuz : " + 3;
+            }
+            else if (ortalama < 55 && ortalama >= 45)
+            {
+                lblTotal.Text = "Ortalamanız : " + ortalama + " Notunuz : " + 2;
+            }
+            else if (ortalama < 45 && ortalama >= 25)
+            {
+                lblTotal.Text = "Ortalamanız : " + ortalama + " Notunuz : " + 1;
+            }
+            else if (ortalama < 25 && ortalama >= 0)
+            {
+                lblTotal.Text = "Ortalamanız : " + ortalama + " Notunuz : " + 0;
+            }
+        }
+    }
+}
